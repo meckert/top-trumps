c0d02b44654fad469dc1706926af622be78c7b7c
meckert <mathias.eckert@autoscout24.com>
2016-11-26 21:13:59 +0100

    WIP: data binding and CSS experiments

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,13 @@
 var app = new Vue({
-    el: '#app',
+    el: '#card',
     data: {
-        message: 'Hello Vue!'
+        img: 'http://c7.nrostatic.com/sites/default/files/uploaded/donald-trump-bankruptcy-lies-r.jpg',
+        items: [
+            { label: 'Price', value: '10.000 €'},
+            { label: 'Mileage', value: '100.000 KM'},
+            { label: 'Registration', value: '01.10.2010'},
+            { label: 'Power', value: '100 kW / 140 HP'},
+            { label: 'Displacement', value: '3799'}
+        ]
     }
-})
+});
