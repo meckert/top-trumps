de1ca204cdd150d2ec980e2a224e4c7f81ac1341
meckert <mathias.eckert@autoscout24.com>
2016-11-27 18:00:18 +0100

    setting background for selected item

--- src/js/index.js
+++ src/js/index.js
@@ -3,11 +3,20 @@ var app = new Vue({
     data: {
         img: 'http://c7.nrostatic.com/sites/default/files/uploaded/donald-trump-bankruptcy-lies-r.jpg',
         items: [
-            { label: 'Price', value: '10.000 €'},
-            { label: 'Mileage', value: '100.000 KM'},
-            { label: 'Registration', value: '01.10.2010'},
-            { label: 'Power', value: '100 kW / 140 HP'},
-            { label: 'Displacement', value: '3799'}
+            { label: 'Price', value: '10.000 €', selected: false },
+            { label: 'Mileage', value: '100.000 KM', selected: false },
+            { label: 'Registration', value: '01.10.2010', selected: false },
+            { label: 'Power', value: '100 kW / 140 HP', selected: false },
+            { label: 'Displacement', value: '3799', selected: false }
         ]
+    },
+    methods: {
+        toggleSelected: function(item) {
+            this.items.forEach(function (item) {
+                item.selected = false;
+            });
+
+            item.selected = !item.selected;
+        }
     }
 });
